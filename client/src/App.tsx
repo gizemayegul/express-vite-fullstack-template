f8d29324a67b7a32262b45c12ed70a3ef789401c
Gizem <109289446+gizemayegul@users.noreply.github.com>
2023-12-28 11:48:02 +0100

    ts.config

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,9 +3,12 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 const API_URL = import.meta.env.VITE_SERVER_URL;
 console.log(API_URL);
+interface myData {
+  data: string;
+}
 
 function App() {
-  const [data, setData] = useState("");
+  const [data, setData] = useState<string>("");
 
   useEffect(() => {
     (async function () {
